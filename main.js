68962e47741671d66ae1a49542523e1f2b0b8cab
Keshav3003 <117528968+Keshav3003@users.noreply.github.com>
2023-07-17 16:35:24 +0530

    Added Responsiveness to site
    Added Responsiveness to site by adding all the media queries and implemented "Hamburger menu" for smaller devices

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+
+/* For Dark Mode Toggle */
+
 var content = document.getElementsByTagName('body')[0];
 var darkMode = document.getElementById('dark-change');
 darkMode.addEventListener('click', function(){
     darkMode.classList.toggle('active');
     content.classList.toggle('night');
+})
+
+/*For Hamburger Menu of smaller Devices */
+
+const toggleButton = document.getElementById('Togglemenu');
+const navLinks = document.getElementById('Toggling');
+
+toggleButton.addEventListener('click',()=>{
+    navLinks.classList.toggle('active');
 })
